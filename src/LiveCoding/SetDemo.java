c53ee92ffdab5ee1559ff661c58e5c24d9ab6691
YunJuHyung <wngud5289@naver.com>
2023-05-14 12:31:18 +0900

    Set framework

---
+++ src/LiveCoding/SetDemo.java
@@ -0,0 +1,38 @@
+package LiveCoding;
+
+import java.util.Iterator;
+import java.util.HashSet;
+
+public class SetDemo {
+    public static void main(String[] args) {
+        HashSet<Integer> A = new HashSet<>();
+        A.add(1);
+        A.add(2);
+        A.add(3);
+
+        HashSet<Integer> B = new HashSet<Integer>();
+        B.add(3);
+        B.add(4);
+        B.add(5);
+
+        HashSet<Integer> C = new HashSet<>();
+        C.add(1);
+        C.add(2);
+
+        // A.addAll(B);     A에 B 합집합
+        //A.retainAll(B);     A 와 B의 교집함
+        //A.removeAll(B);   A - B 차집합
+
+        System.out.println(A.containsAll(B)); //false
+        System.out.println(A.containsAll(C)); //true
+
+        Iterator hi = (Iterator) A.iterator();
+        while(hi.hasNext()){
+            System.out.println(hi.next());
+
+
+        }
+
+    }
+}
+
